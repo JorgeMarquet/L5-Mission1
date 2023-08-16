821f1775bd4e2b1e9029a4b13c20db38e71fa2ce
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-08-16 15:29:35 +1200

    amended main branch to avoid potential future merging conflicts with other branches

--- server.js
+++ server.js
@@ -37,58 +37,6 @@ server.get("./", (req, res) => {
     
 })
 
-// Helper function to calculate risk rating
-function calculateRiskRating(claimHistory) {
-    const keywords = ["collide", "crash", "scratch", "bump", "smash"];
-    let count = 0;
-
-    keywords.forEach(keyword => {
-        const regex = new RegExp(keyword, 'gi');
-        const matches = claimHistory.match(regex);
-        if (matches) {
-            count += matches.length;
-        }
-    });
-
-    // Clamp the value between 1 and 5
-    return Math.min(Math.max(count, 1), 5);
-}
-
-// API Endpoint 2
-server.post('/api/risk-rating', (req, res) => {
-    const claimHistory = req.body.claim_history;
-
-    if (typeof claimHistory !== 'string' || claimHistory.length === 0) {
-        return res.json({ error: "there is an error" });
-    }
-
-    const riskRating = calculateRiskRating(claimHistory);
-    res.json({ risk_rating: riskRating });
-});
-
-// Helper function to calculate premiums
-function calculatePremiums(carValue, riskRating) {
-    const yearlyPremium = (carValue * riskRating) / 100;
-    const monthlyPremium = yearlyPremium / 12;
-    
-    return {
-        monthly_premium: monthlyPremium,
-        yearly_premium: yearlyPremium
-    };
-}
-
-// API Endpoint 3
-server.post('/api/quote', (req, res) => {
-    const carValue = parseFloat(req.body.car_value);
-    const riskRating = parseInt(req.body.risk_rating);
-
-    if (isNaN(carValue) || isNaN(riskRating) || riskRating < 1 || riskRating > 5) {
-        return res.json({ error: "there is an error" });
-    }
-
-    const premiums = calculatePremiums(carValue, riskRating);
-    res.json(premiums);
-});
 
 const port = 8000;
 server.listen(port,() => {
